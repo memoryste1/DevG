4be8c02bef8dfe0fde0b8656105e792cfd09ae5f
digiacomth <digiacomth@ES-STE2-M409.etmlnet.local>
2019-06-17 15:52:41 +0200

    Test hashmap

--- Memory/src/memory/Memory.java
+++ Memory/src/memory/Memory.java
@@ -5,16 +5,35 @@
  */
 package memory;
 
+import java.util.HashMap;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+
 /**
  *
  * @author Slyvain
  */
 public class Memory {
-
+    HashMap<String, Object> map = new HashMap<String, Object>();
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
     }                
+    
+    /***
+     * @author Théo
+     */
+    public void getPictures(){
+        int i = map.size();
+        i++;
+        String url="./"+i+".png";
+        //map.put(map.size(), ImageIO.read(getClass().getResource("/images/c.png")));
+    }
 }
