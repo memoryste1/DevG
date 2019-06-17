9822ce85bae76fd59e84d98412f9fe95ab1d05fd
digiacomth <digiacomth@ES-STE2-M409.etmlnet.local>
2019-06-17 16:23:34 +0200

    Memory.java cleaned

--- Memory/src/memory/Memory.java
+++ Memory/src/memory/Memory.java
@@ -5,13 +5,6 @@
  */
 package memory;
 
-import java.util.HashMap;
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 
 
 /**
@@ -19,7 +12,7 @@ import javax.imageio.ImageIO;
  * @author Slyvain
  */
 public class Memory {
-    HashMap<String, Object> map = new HashMap<String, Object>();
+    
 
      /**
     * @param args the command line arguments
@@ -33,13 +26,5 @@ public class Memory {
       });
    }
     
-    /***
-     * @author Théo
-     */
-    public void getPictures(){
-        int i = map.size();
-        i++;
-        String url="./"+i+".png";
-        //map.put(map.size(), ImageIO.read(getClass().getResource("/images/c.png")));
-    }
+    
 }
