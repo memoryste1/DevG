72f107dbbe11c9e14d621a226fb51ed9c4c2bcbd
besanconsy <besanconsy@ES-STE1-M416.etmlnet.local>
2019-07-01 15:06:57 +0200

    asdasdasdasdasd

--- Memory/src/memory/MainFrame.java
+++ Memory/src/memory/MainFrame.java
@@ -6,6 +6,7 @@
 package memory;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
@@ -22,6 +23,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import java.io.*;
+import java.text.Normalizer;
 import javafx.animation.Animation;
 import javax.swing.Icon;
 import javax.swing.JFrame;
@@ -35,24 +37,36 @@ import javax.swing.SwingUtilities;
  */
 public class MainFrame extends javax.swing.JFrame {
 
+    // Variables declaration - do not modify                     
+    private java.awt.Panel panel1;
+    // End of variables declaration                   
+    private JPanel pnlGrid;
+    private JPanel pnlConf;
+    private Timer timer;
+
+    private final ImageIcon imageZero = new ImageIcon(getClass().getResource("./nu.png"));
+    private final ImageIcon imageZeroRoll = new ImageIcon(getClass().getResource("./nuB.png"));
+
     //Hash map of number
-    HashMap<String, String> map = new HashMap<String, String>();
+    private HashMap<String, String> map = new HashMap<String, String>();
 
     //Get the hashmap first clicked card's key
     String firstCardkey = "";
     ActionEvent firstTouchedButton;
     ActionEvent secondTouchedButton;
-    ImageIcon imageZero = new ImageIcon(getClass().getResource("./nu.png"));
-    ImageIcon imageZeroRoll = new ImageIcon(getClass().getResource("./nuB.png"));
+
     /**
      * load pictures in hashmap
      *
      * @author Théo
      */
     public void loadPictures(int numberOfCard) {
+
         Random rand = new Random();
-        boolean flag;
-        int n;
+        boolean flag = false;
+        int n = 0;
+
+        int startCard = rand.nextInt(31 - numberOfCard);
 
         for (int i = 0; i < numberOfCard; i++) {
             for (int j = 0; j < 2; j++) {
@@ -67,7 +81,8 @@ public class MainFrame extends javax.swing.JFrame {
                     }
                 } while (flag != true);
 
-                String text = "./" + i + ".png";
+                int start = i + startCard;
+                String text = "./" + start + ".png";
                 map.put(String.valueOf(n), text);
             }
         }//End for
@@ -78,23 +93,34 @@ public class MainFrame extends javax.swing.JFrame {
      */
     public MainFrame() {
         int lastCard = 0;
-        int rows = 4;
-        int cols = 1;
-        int cardNumber = 14;
+        int cardNumber = 5;
+
+        int rows = 2;
+        for (int i = 3; i <= (int) Math.sqrt(cardNumber * 2); i++) {
+            if ((cardNumber * 2) % i == 0) {
+                rows = i;
+            }
+        }
+
+        int cols = 0;
+
         int vgap = 0;
-        
-        timer = new Timer(1000, new TimerListener());
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        timer = new Timer(600, new TimerListener());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
         loadPictures(cardNumber);
         GridLayout GridMemory = new GridLayout(rows, cols);
+        this.setMinimumSize(new Dimension((rows * 90), (cardNumber * 2 / rows) * 100));
 
         pnlGrid = new JPanel();
         pnlConf = new JPanel();
 
         this.add(pnlConf, BorderLayout.SOUTH);
         this.add(pnlGrid, BorderLayout.CENTER);
-        this.setBounds(100, 100, 800, 600);
+
+        //Dimentionnement du plateau de jeu
+        this.setBounds(100, 100, (rows * 90),(cardNumber * 2 / rows) * 100);
         pnlGrid.setLayout(GridMemory);
 
         //this.setContentPane(pnlMemory);
@@ -110,7 +136,7 @@ public class MainFrame extends javax.swing.JFrame {
             currentBtn.setIcon(imageZero);
             currentBtn.setRolloverIcon(imageZeroRoll);
             currentBtn.setRolloverEnabled(true);
-      
+
             pnlGrid.add(currentBtn);
 
         }
@@ -123,21 +149,21 @@ public class MainFrame extends javax.swing.JFrame {
         });
     }
 
-    class TimerListener implements ActionListener{
+    class TimerListener implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent ae) {
+
             ((JButton) secondTouchedButton.getSource()).setIcon(imageZero);
             ((JButton) firstTouchedButton.getSource()).setIcon(imageZero);
             ((JButton) firstTouchedButton.getSource()).setEnabled(true);
             ((JButton) secondTouchedButton.getSource()).setEnabled(true);
+
             firstCardkey = "";
             secondTouchedButton = null;
             firstTouchedButton = null;
         }
-        
-        
-        
+
     }
 
     class BtnListener implements ActionListener {
@@ -145,82 +171,79 @@ public class MainFrame extends javax.swing.JFrame {
         @Override
         @SuppressWarnings("empty-statement")
         public void actionPerformed(ActionEvent e) {
-        //Check if it have 2 button clicked    
-        if(secondTouchedButton == null){
-            timer.stop();
+            //Check if it have 2 button clicked    
+            if (secondTouchedButton == null) {
+                timer.stop();
 
-            // if the pressed button is the first one then:
-            if (firstCardkey == "") {
+                // if the pressed button is the first one then:
+                if (firstCardkey == "") {
 
-                //keap the button's name (int in var String)
-                firstCardkey = ((JButton) e.getSource()).getName();
-                ImageIcon imageForOne = new ImageIcon(getClass().getResource(map.get(firstCardkey)));
-
-                //Change visible button's text
-                ((JButton) e.getSource()).setDisabledIcon(imageForOne);
-
-                //Set this button disabled
-                ((JButton) e.getSource()).setEnabled(false);
-
-                //Save the object in firstTouchedButton
-                firstTouchedButton = e;
-                
-            } //Else if the pressed button is the second one then:
-            else {
-
-                //If the first and second button's are same
-                if (map.get(firstCardkey).equals(map.get(((JButton) e.getSource()).getName()))) {
-
-                    //Set second button visible and disabled it
-                    ImageIcon imageForOne = new ImageIcon(getClass().getResource(map.get(((JButton) e.getSource()).getName())));
+                    //keap the button's name (int in var String)
+                    firstCardkey = ((JButton) e.getSource()).getName();
+                    ImageIcon imageForOne = new ImageIcon(getClass().getResource(map.get(firstCardkey)));
 
                     //Change visible button's text
                     ((JButton) e.getSource()).setDisabledIcon(imageForOne);
+
+                    //Set this button disabled
                     ((JButton) e.getSource()).setEnabled(false);
-                    firstCardkey = "";
-                    firstTouchedButton = null;
-                    secondTouchedButton = null;
-                } //Else if the first and second buttons aren't same
+
+                    //Save the object in firstTouchedButton
+                    firstTouchedButton = e;
+
+                } //Else if the pressed button is the second one then:
                 else {
 
-                    if(firstTouchedButton != null){
-                    secondTouchedButton = e;
-                    }
-                    
-                    ImageIcon temp= new ImageIcon(getClass().getResource(map.get(((JButton) e.getSource()).getName())));
-                    ((JButton) e.getSource()).setDisabledIcon(temp);
-                    
-                    
-                    
-                    
-                    ((JButton) e.getSource()).setEnabled(false);
-                    
-                    
-                    
-                    timer.start();
-  
-                }//End else
-                boolean checkFinish = true;
-                    for(int i = 0; i < pnlGrid.getComponentCount(); i++){  
-                        if(((JButton) pnlGrid.getComponent(i)).isEnabled()){
+                    //If the first and second button's are same
+                    if (map.get(firstCardkey).equals(map.get(((JButton) e.getSource()).getName()))) {
+
+                        //Set second button visible and disabled it
+                        ImageIcon imageForOne = new ImageIcon(getClass().getResource(map.get(((JButton) e.getSource()).getName())));
+
+                        //Change visible button's text
+                        ((JButton) e.getSource()).setDisabledIcon(imageForOne);
+                        ((JButton) e.getSource()).setEnabled(false);
+                        firstCardkey = "";
+                        firstTouchedButton = null;
+                        secondTouchedButton = null;
+                    } //Else if the first and second buttons aren't same
+                    else {
+
+                        if (firstTouchedButton != null) {
+                            secondTouchedButton = e;
+                        }
+
+                        ImageIcon temp = new ImageIcon(getClass().getResource(map.get(((JButton) e.getSource()).getName())));
+                        ((JButton) e.getSource()).setDisabledIcon(temp);
+
+                        ((JButton) e.getSource()).setEnabled(false);
+
+                        timer.start();
+
+                    }//End else
+                    boolean checkFinish = true;
+                    for (int i = 0; i < pnlGrid.getComponentCount(); i++) {
+                        if (((JButton) pnlGrid.getComponent(i)).isEnabled()) {
                             checkFinish = false;
                         }
                     }
-                    if(checkFinish){
-                        int choice=JOptionPane.showConfirmDialog(new JFrame(),"Bien joué! Voulez-vous recommencer?");  
-                        if(choice==JOptionPane.YES_OPTION){  
+                    if (checkFinish) {
+                        int choice = JOptionPane.showConfirmDialog(new JFrame(), "Bien joué! Voulez-vous recommencer?");
+                        if (choice == JOptionPane.YES_OPTION) {
                             new MainFrame().setVisible(true);
+                            
                             dispose();
                         }
-                        if(choice==JOptionPane.NO_OPTION){  
+                        if (choice == JOptionPane.NO_OPTION) {
+                            System.out.println(getParent().getClass());
                             System.exit(0);
+                            
                         }
-                        
-                }
-            }//End if
-        }
-        }//End actionPerformed
 
+                    }
+                }//End if
+            }
+        }//End actionPerformed
 
     }//End btnListener
 
@@ -298,10 +321,4 @@ public class MainFrame extends javax.swing.JFrame {
         });
     }
 
-    // Variables declaration - do not modify                     
-    private java.awt.Panel panel1;
-    // End of variables declaration                   
-    private JPanel pnlGrid;
-    private JPanel pnlConf;
-    private Timer timer;
 }
